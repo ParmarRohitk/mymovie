cc5353c571af3c29b705840b0bbfc7233b059e69
Parmar Rohit k. <86903998+ParmarRohitk@users.noreply.github.com>
2025-01-04 23:01:37 +0530

    remove unused

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Script from "next/script";
+// import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
